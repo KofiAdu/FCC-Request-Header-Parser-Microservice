420c48e6ff169a3357e2684de90ca59d3181696b
Kofi <kofi@ymail.com>
2022-02-13 17:42:25 +0200

    change

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get("/api/hello", function (req, res) {
 });
 
 app.get("/api/whoami", (req,res)=>{
-  res.json({ipaddress: req.socket.address().address,language: req.headers['accept-language'],
+  res.json({ipaddress: req.connection.remoteAddress,language: req.headers['accept-language'],
   software: req.headers['user-agent'], })
 })
 
