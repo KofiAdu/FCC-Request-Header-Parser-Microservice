3633816cd1e41d98fd9abc6265b88137e62f28a2
Kofi <kofi@ymail.com>
2022-02-13 16:50:09 +0200

    solution

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get("/api/hello", function (req, res) {
 });
 
 app.get("/api/whoami", (req,res)=>{
-  res.json({ipaddress: req.socket.remoteAddress,language: req.headers['accept-language'],
+  res.json({ipaddress: req.headers['x-forwarded-for'],language: req.headers['accept-language'],
   software: req.headers['user-agent'], })
 })
 
